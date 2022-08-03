63887cb355247ad14ae4fb86c468d987f0ff1b1f
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-08-03 21:35:41 +0100

    form styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Mustafa's Payroll</h1>
+      <div className="inputs">
+        <label>Name</label>
+        <input type="text" />
+        <label>Age</label>
+        <input type="Number" />
+        <label>Salary (yearly):</label>
+        <input type="Number" />
+        <label>Postion</label>
+        <input type="text" />
+        <button className="btn">Add Employee</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
