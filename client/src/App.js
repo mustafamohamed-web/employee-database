7a52f48a8f309960c6f584c9a8f76daa054be6bf
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-08-03 22:01:11 +0100

    set up state

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [postion, setPosition] = useState("");
+  const [salary, setSalary] = useState(0);
+
+  const handleClick = () => {
+    console.log(name + age + salary + postion);
+  };
   return (
     <div className="App">
       <h1>Mustafa's Payroll</h1>
       <div className="inputs">
         <label>Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <label>Age</label>
-        <input type="Number" />
+        <input
+          type="Number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
         <label>Salary (yearly):</label>
-        <input type="Number" />
+        <input
+          type="Number"
+          onChange={(e) => {
+            setSalary(e.target.value);
+          }}
+        />
         <label>Postion</label>
-        <input type="text" />
-        <button className="btn">Add Employee</button>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <button onClick={handleClick} className="btn">
+          Add Employee
+        </button>
       </div>
     </div>
   );
