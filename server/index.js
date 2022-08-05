c11161ff425b4a4d3f242bd298ed6092541dbc61
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-08-05 15:45:29 +0100

    databse connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "mustafa246",
+  database: "employee-database",
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const position = req.body.position;
+  const salary = req.body.salary;
+
+  db.query(
+    "INSERT INTO employee-database (name,age,position,salary) VALUES (?,?,?,?),",
+    [name, age, position, salary],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
 
 app.listen(3001, () => {
   console.log("server is running");
