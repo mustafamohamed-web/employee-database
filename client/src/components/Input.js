d986430e904983bbbfce0434ddc6277f72c6d22e
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-08-05 22:23:01 +0100

    connected to backend

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import "../styles/Input.css";
+import Axios from "axios";
 
 const Input = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
-  const [postion, setPosition] = useState("");
+  const [position, setPosition] = useState("");
   const [salary, setSalary] = useState(0);
 
-  const handleClick = () => {
-    console.log(name + age + salary + postion);
+  // const handleClick = () => {
+  //   console.log(name + age + salary + postion);
+  // };
+
+  const createEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      position: position,
+      salary: salary,
+    }).then(() => {
+      console.log("success");
+    });
   };
+
   return (
     <div>
       <h1>Mustafa's Payroll</h1>
@@ -28,21 +41,21 @@ const Input = () => {
             setAge(e.target.value);
           }}
         />
-        <label>Salary (yearly):</label>
+        <label>Position</label>
         <input
-          type="Number"
-          onChange={(e) => {
-            setSalary(e.target.value);
-          }}
-        />
-        <label>Postion</label>
-        <input
-          type="text"
+          type="Text"
           onChange={(e) => {
             setPosition(e.target.value);
           }}
         />
-        <button onClick={handleClick} className="btn">
+        <label>Salary</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setSalary(e.target.value);
+          }}
+        />
+        <button onClick={createEmployee} className="btn">
           Add Employee
         </button>
       </div>
