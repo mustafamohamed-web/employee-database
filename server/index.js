c45b248413e61e747367213aa1d5f86bb42f1816
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-08-07 15:11:24 +0100

    delete request completed

--- server/index.js
+++ server/index.js
@@ -59,6 +59,18 @@ app.put("/update", (req, res) => {
   );
 });
 
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM employees WHERE 'id' = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("Values Inserted");
+    }
+  });
+});
+
 app.listen(3001, () => {
   console.log("server is running");
 });
